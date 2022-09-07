9b3eff25f19e305593859c87b2f72980ad006b5d
Saffr3n <saffr3n@gmail.com>
2022-09-07 17:24:05 +0300

    Edit script.js. The game has full functionality, but in the console for now

--- script.js
+++ script.js
@@ -18,18 +18,47 @@ function getComputerChoice() {
 function playRound (playerSelection, computerSelection) {
     let diff = playerSelection - computerSelection;
 
-    if (Math.abs(diff) === 2) {
+    if (Math.abs(diff) === 2)
         diff /= -2;
+
+    return diff;
+}
+
+function game() {
+    let playerSelection;
+    let computerSelection;
+
+    let playerScore = 0;
+    let computerScore = 0;
+    
+    for (let i = 1; i <= 5; i++) {
+        playerSelection = getPlayerChoice();
+        computerSelection = getComputerChoice();
+
+        console.log(`Round ${i}:\n`)
+
+        const diff = playRound(playerSelection, computerSelection);
+
+        switch (diff) {
+            case 0:
+                console.log('Draw!\n\n');
+                break;
+            case 1:
+                console.log(`You win! ${numToName(playerSelection)} ${playerSelection === 2 ? 'beat' : 'beats'} ${numToName(computerSelection)}\n\n`);
+                playerScore++;
+                break;
+            default:
+                console.log(`You lose! ${numToName(computerSelection)} ${computerSelection === 2 ? 'beat' : 'beats'} ${numToName(playerSelection)}\n\n`);
+                computerScore++;
+        }
     }
 
-    switch (diff) {
-        case 0:
-            return 'Draw!';
-        case 1:
-            return `You win! ${numToName(playerSelection)} ${playerSelection === 2 ? 'beat' : 'beats'} ${numToName(computerSelection)}`;
-        default:
-            return `You lose! ${numToName(computerSelection)} ${computerSelection === 2 ? 'beat' : 'beats'} ${numToName(playerSelection)}`;
-    }
+    if (playerScore > computerScore)
+        console.log(`You won the game with the score of ${playerScore} against the enemy score of ${computerScore}!`);
+    else if (playerScore < computerScore)
+        console.log(`You lost the game with the score of ${playerScore} against the enemy score of ${computerScore}!`);
+    else
+        console.log(`Draw! Both players scored ${playerScore}`);
 }
 
 function numToName (number) {
@@ -43,4 +72,4 @@ function numToName (number) {
     }
 }
 
-console.log(playRound(getPlayerChoice(), getComputerChoice()));
+game();
